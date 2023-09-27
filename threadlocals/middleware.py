03f7f0767a03ee51fa1d0bd985fe61a8ad5dff04
Denis Ziegler <denis@nutrislice.com>
2023-09-27 15:59:59 -0600

    https://nutrislice.atlassian.net/browse/ENG-4987

--- threadlocals/middleware.py
+++ threadlocals/middleware.py
@@ -25,6 +25,8 @@ class ThreadLocalMiddleware(MiddlewareMixin):
     """Middleware that puts the request object in thread local storage."""
 
     def process_request(self, request):
+        if request.path in ['/heartbeat']:
+            return None
         set_thread_variable('request', request)
         # set_current_user(request.user) # not going to store user in TL's for now, since we can get it from the request if we need it, and I read somewhere that accessing reqeust.user can potentially prevent view caching from functioning correctly
 
